5605cdcb257c7050a9ba6d1db967a545f1530785
MIchaelSciole412 <msciole@outlook.com>
2024-08-26 15:45:16 -0400

    Added SillyWords.py as a joke

---
+++ SillyWords.py
@@ -0,0 +1,6 @@
+from random import randrange
+
+
+silly_words = ["Bamboozled: To have been tricked", "Hullaballoo: A Large Commotion", "Bumbershoot: An Umbrella", "Biblioklept: A book thief", "Flabbergast: To be Surprised", "Floccinaucinihilipilification: Estimating something as worthless...like this piece of code"]
+
+print(silly_words[randrange(6)])
